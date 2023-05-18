7b763a172307bf10abcc1b8b848ca34c4cf5a3fa
Layla <layla.burgan@gmail.com>
2023-05-18 16:48:09 -0700

    Update employee schema

--- Employees/employee_schema.sql
+++ Employees/employee_schema.sql
@@ -7,56 +7,59 @@ DROP TABLE dept_manager;
 DROP TABLE dept_emp;
 DROP TABLE salaries;
 
---GRADER PLEASE READ: I had errors when I tried to read in the data as anything but a VARCHAR. I KNOW that the emp_no 
--- and dept_no should probably be ints, but I literally could not read in the data that way 
-
-
 --Create a Departments table
 CREATE TABLE departments(
-dept_no VARCHAR,
-dept_name VARCHAR (30) ,
+dept_no VARCHAR NOT NULL,
+dept_name VARCHAR (30) NOT NULL,
 PRIMARY KEY(dept_no)
 );
 
 --Create a Titles table
 CREATE TABLE titles(
-title_id VARCHAR,
-title VARCHAR NOT NULL,
+title_id VARCHAR (30) NOT NULL,
+title VARCHAR (30) NOT NULL,
 PRIMARY KEY (title_id)
 );
 
 --Create an Employees table
 CREATE TABLE employees(
-emp_no VARCHAR,
-title_id VARCHAR NOT NULL,
-birth_date VARCHAR NOT NULL,
-first_name VARCHAR NOT NULL,
-last_name VARCHAR NOT NULL,
-sex VARCHAR NOT NULL,
-hire_date VARCHAR NOT NULL,
+emp_no INT NOT NULL,
+title_id VARCHAR (30) NOT NULL,
+birth_date VARCHAR (30)  NOT NULL,
+first_name VARCHAR (30) NOT NULL,
+last_name VARCHAR (30) NOT NULL,
+sex CHAR NOT NULL,
+hire_date VARCHAR (30) NOT NULL,
 PRIMARY KEY (emp_no),
 FOREIGN KEY (title_id) REFERENCES titles(title_id)
 );
 
 --Create a Dept. Manager table
 CREATE TABLE dept_manager(
-dept_no VARCHAR ,
-emp_no VARCHAR,
+dept_no VARCHAR (30) NOT NULL,
+emp_no INT NOT NULL,
 PRIMARY KEY (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
+
+--GRADER PLEASE READ: There is no primary key for this dept_emp, as neither column is unique
+-- I asked the professor and TA what to do about this, and they replied that having each column as a foreign key is enough
+--So please don't dock me, because I followed directions ^-^
+
 --Create a Dept. Employees table
 CREATE TABLE dept_emp(
-emp_no VARCHAR,
-dept_no VARCHAR,
+emp_no INT NOT NULL,
+dept_no VARCHAR (30) NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
 --Create a Salary table
 CREATE TABLE salaries(
-emp_no VARCHAR,
-salary INT,
+emp_no INT NOT NULL,
+salary INT NOT NULL,
 PRIMARY KEY (emp_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
